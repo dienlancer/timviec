69952ef76e7edaa4b938b914e05511067f7db22f
trietnk01 <trietnk01@gmail.com>
2018-03-30 01:30:47 +0700

    cap nhat

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -27,34 +27,5 @@ $company=$setting['contacted_person']['field_value'];
 		</div>
 	</div>	
 </div>
-<div class="container margin-top-15">
-	<div class="row">
-		<div class="col-lg-2 dk">
-			<a  href="javascript:void(0);">
-				<div><center><i class="fas fa-sign-in-alt"></i></center></div>
-				<div><center>ĐĂNG NHẬP</center> </div>
-			</a>
-		</div>
-		<div class="col-lg-2 dk">
-			<a  href="javascript:void(0);">
-				<div><center><i class="fas fa-lock"></i></center></div>
-				<div><center>ĐĂNG KÝ</center> </div>
-			</a>
-		</div>		
-		<div class="col-lg-4 tao-ho-so">
-			<a  href="javascript:void(0);">
-				<div class="row">
-					<div class="col-xs-6">
-						<div class="ths">TẠO HỒ SƠ</div>
-						<div class="sloganthsmp">Tạo hồ sơ miễn phí, có ngay việc làm ưng ý</div>
-					</div>
-					<div class="col-xs-6">
-						<div class="mica"><i class="fas fa-address-book"></i></div>
-					</div>
-				</div>
-			</a>
-		</div>
-		<div class="col-lg-4 dang-tin-mien-phi"></div>
-	</div>
-</div>
+@include("frontend.content-top-register")
 @endsection()               

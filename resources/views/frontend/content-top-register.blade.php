699443b886d08756cc74cfcf59f07139aa51d4d6
trietnk01 <trietnk01@gmail.com>
2018-04-05 12:18:12 +0700

    cập nhật

--- resources/views/frontend/content-top-register.blade.php
+++ resources/views/frontend/content-top-register.blade.php
@@ -14,12 +14,12 @@
 		</div>		
 		<div class="col-lg-4 tao-ho-so">
 			<a  href="javascript:void(0);">
-				<div class="row">
-					<div class="col-xs-6">
+				<div class="zidan">
+					<div >
 						<div class="ths">TẠO HỒ SƠ</div>
 						<div class="sloganthsmp">Tạo hồ sơ miễn phí, có ngay việc làm ưng ý</div>
 					</div>
-					<div class="col-xs-6">
+					<div >
 						<div class="mica"><i class="fas fa-address-book"></i></div>
 					</div>
 				</div>
@@ -27,13 +27,13 @@
 		</div>
 		<div class="col-lg-4 dang-tin-mien-phi">
 			<a  href="javascript:void(0);">
-				<div class="row">
-					<div class="col-xs-6">
+				<div class="zidan">
+					<div >
 						<div class="ths">ĐĂNG TIN MIỄN PHÍ</div>
 						<div class="sloganthsmp">Tiếp cận nhanh nhất với hơn 1 triệu hồ sơ tìm việc</div>
 					</div>
-					<div class="col-xs-6">
-						<div class="mica"><i class="far fa-address-card"></i></div>
+					<div >
+						<div class="mica-2"><i class="far fa-address-card"></i></div>
 					</div>
 				</div>
 			</a>

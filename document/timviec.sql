d36d4447f12aca7377076b20e39d959812bc9529
trietnk01 <trietnk01@gmail.com>
2018-04-26 18:22:55 +0700

    cập nhật

--- document/timviec.sql
+++ document/timviec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 26, 2018 lúc 10:57 AM
+-- Thời gian đã tạo: Th4 26, 2018 lúc 01:21 PM
 -- Phiên bản máy phục vụ: 10.1.29-MariaDB
 -- Phiên bản PHP: 7.0.26
 

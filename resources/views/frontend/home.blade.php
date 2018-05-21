dfe01a376c6a8bfc740933a4cb36c019a550dcec
NguyenKimDien <trietnk01@gmail.com>
2018-05-21 18:18:04 +0700

    cập nhật

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -25,12 +25,12 @@ $height=$setting['product_height']['field_value'];
 @include("frontend.content-top-register")
 <div class="container">
 	<?php 
-	$query_hot_job=DB::table('recruitment')
+	$query_new_job=DB::table('recruitment')
 	->join('employer','recruitment.employer_id','=','employer.id')
 	->join('salary','recruitment.salary_id','=','salary.id');
-	$query_hot_job->where('recruitment.status',1);
-	$query_hot_job->where('recruitment.status_employer',1);
-	$source_hot_job=$query_hot_job->select(
+	$query_new_job->where('recruitment.status',1);
+	$query_new_job->where('recruitment.status_employer',1);
+	$source_new_job=$query_new_job->select(
 		'recruitment.id',
 		'recruitment.fullname',
 		'recruitment.alias',
@@ -54,7 +54,7 @@ $height=$setting['product_height']['field_value'];
 	->take(63)
 	->get()
 	->toArray();		
-	if(count($source_hot_job) > 0){
+	if(count($source_new_job) > 0){
 		?>
 		<div class="row">
 			<div class="col-lg-12">
@@ -70,30 +70,30 @@ $height=$setting['product_height']['field_value'];
 		</div>	
 		<div class="row">	
 			<?php 
-			$data_hot_job=convertToArray($source_hot_job);
+			$data_new_job=convertToArray($source_new_job);
 			$k=1;
-			foreach ($data_hot_job as $key => $value) {
-				$hot_job_fullname=truncateString($value['fullname'],40) ;
-				$hot_job_employer=truncateString($value['employer_fullname'],40);
-				$hot_job_duration=datetimeConverterVn($value['duration']);
-				$hot_job_img='';
+			foreach ($data_new_job as $key => $value) {
+				$new_job_fullname=truncateString($value['fullname'],40) ;
+				$new_job_employer=truncateString($value['employer_fullname'],40);
+				$new_job_duration=datetimeConverterVn($value['duration']);
+				$new_job_img='';
 				if(!empty($value['logo'])){
-					$hot_job_img=asset('upload/'.$width.'x'.$height.'-'.$value['logo']);
+					$new_job_img=asset('upload/'.$width.'x'.$height.'-'.$value['logo']);
 				}else{
-					$hot_job_img=asset('upload/no-logo.png');
+					$new_job_img=asset('upload/no-logo.png');
 				}
 				?>
 				<div class="col-lg-4">
 					<div class="hot-job-box">
 						<div class="hot-job-img">
-							<img src="<?php echo $hot_job_img; ?>" width="64">
+							<img src="<?php echo $new_job_img; ?>" width="64">
 						</div>
 						<div class="hot-job-right">
-							<div class="hot-job-name"><a title="<?php echo $value['fullname']; ?>" href="<?php echo route('frontend.index.index',[$value['alias']]); ?>"><?php echo $hot_job_fullname; ?></a></div>
-							<div class="hot-job-employer"><a title="<?php echo $value['employer_fullname']; ?>" href="<?php echo route('frontend.index.index',[$value['employer_alias']]); ?>"><?php echo $hot_job_employer; ?></a></div>
+							<div class="hot-job-name"><a title="<?php echo $value['fullname']; ?>" href="<?php echo route('frontend.index.index',[$value['alias']]); ?>"><?php echo $new_job_fullname; ?></a></div>
+							<div class="hot-job-employer"><a title="<?php echo $value['employer_fullname']; ?>" href="<?php echo route('frontend.index.index',[$value['employer_alias']]); ?>"><?php echo $new_job_employer; ?></a></div>
 							<div>
 								<div class="simantest-left"><i class="far fa-money-bill-alt"></i>&nbsp;<?php echo $value['salary_name']; ?></div>
-								<div class="simantest-right">Ngày hết hạn:&nbsp;<?php echo $hot_job_duration; ?></div>
+								<div class="simantest-right">Ngày hết hạn:&nbsp;<?php echo $new_job_duration; ?></div>
 								<div class="clr"></div>
 							</div>
 						</div>		
@@ -101,7 +101,7 @@ $height=$setting['product_height']['field_value'];
 					</div>
 				</div>
 				<?php
-				if($k%3==0 || $k == count($data_hot_job)){
+				if($k%3==0 || $k == count($data_new_job)){
 					echo '<div class="clr"></div>';
 				}
 				$k++;
@@ -338,6 +338,44 @@ $height=$setting['product_height']['field_value'];
 					</div>				
 					<?php
 				}
+				$query_hot_job=DB::table('recruitment')
+				->join('employer','recruitment.employer_id','=','employer.id')
+				->join('salary','recruitment.salary_id','=','salary.id')
+				->join('scale','employer.scale_id','=','scale.id');
+				$query_hot_job->where('recruitment.status',1);
+				$query_hot_job->where('recruitment.status_employer',1);
+				$source_hot_job=$query_hot_job->select(
+					'recruitment.id',
+					'recruitment.fullname',
+					'recruitment.alias',
+					'recruitment.duration',
+					'salary.fullname as salary_name',
+					'employer.fullname as employer_fullname',
+					'employer.address',
+					'scale.fullname as scale_name',
+					'employer.alias as employer_alias',
+					'employer.logo'
+				)
+				->groupBy(
+					'recruitment.id',
+					'recruitment.fullname',
+					'recruitment.alias',
+					'recruitment.duration',
+					'salary.fullname',
+					'employer.fullname',
+					'employer.address',
+					'scale.fullname',
+					'employer.alias',
+					'employer.logo'
+				)
+				->orderBy('recruitment.id', 'desc')
+				->take(63)
+				->get()
+				->toArray();	
+				if(count($source_hot_job) > 0){
+					$data_hot_job=convertToArray($source_hot_job);		
+					echo "<pre>".print_r($data_hot_job,true)."</pre>";			
+				}	
 				?>					
 			</div>
 			<div class="col-lg-4">

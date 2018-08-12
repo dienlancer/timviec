80c953f930db2280bb38d9d6287587817c2e28cc
NguyenKimDien <trietnk01@gmail.com>
2018-08-13 02:46:13 +0700

    cập nhật

--- resources/views/frontend/article.blade.php
+++ resources/views/frontend/article.blade.php
@@ -31,11 +31,13 @@
 				->get()->toArray();						
 				$arr_category_name=array();	
 				$category_name='';	
+				$arr_category_id=array();
 				if(count($source_article_category) > 0){		
 					$data_article_category=convertToArray($source_article_category);
 					foreach ($data_article_category as $key => $value_article_category) {								
+						$arr_category_id[]=$value_article_category["id"];
 						$permalink_category=route('frontend.index.index',[$value_article_category['alias']]);
-						$arr_category_name[]='<span><a href="'.$permalink_category.'">'.$value_article_category["fullname"].'</a></span>' ;				
+						$arr_category_name[]='<span><a href="'.$permalink_category.'">'.$value_article_category["fullname"].'</a></span>' ;			
 					}		
 					$category_name=implode(' / ', $arr_category_name);		
 				}				
@@ -56,16 +58,44 @@
 						</div>
 						<div class="clr"></div>
 						<div class="jp_blog_single_bottom_post_cont">
-							<div><?php echo @$item['content']; ?></div>
-							<ul>
-								<li><i class="fa fa-tags"></i>Keywords :</li>
-								<li><a href="#">ui designer,</a></li>
-								<li><a href="#">developer,</a></li>
-								<li><a href="#">senior</a></li>
-								<li><a href="#">it company,</a></li>
-								<li><a href="#">design,</a></li>
-								<li><a href="#">call center</a></li>
-							</ul>
+							<div><?php echo @$item['content']; ?></div>	
+							<?php 					
+							if(count($arr_category_id) > 0){						
+								$data=DB::table('article')
+								->join('article_category','article.id','=','article_category.article_id')                			
+								->select('article.id','article.alias','article.fullname')
+								->whereIn('article_category.category_id', $arr_category_id)
+								->where('article.id','<>',(int)@$id)
+								->where('article.status','=',1)
+								->groupBy('article.id','article.alias','article.fullname')
+								->orderBy('article.id', 'desc')
+								->take(6)
+								->get()->toArray();
+								$data=convertToArray($data);            
+								?>
+								<div class="margin-top-10">
+									<div>
+										<b>Tin liên quan :</b>
+									</div>
+									<div class="related-news-right">
+										<ul class="related-articles">
+											<?php 
+											foreach ($data as $key => $value_related) {
+												$related_article_fullname=$value_related["fullname"];
+												$related_article_alias=$value_related['alias'];
+												$related_article_permalink=route('frontend.index.index',[$related_article_alias]) ;
+												?>
+												<li><a href="<?php echo $related_article_permalink; ?>"><?php echo $related_article_fullname; ?></a></li>
+												<?php
+											}
+											?>					
+										</ul>
+									</div>	
+									<div class="clr"></div>			
+								</div>	
+								<?php				
+							}
+							?>	
 						</div>
 						<div class="clr"></div>
 						<div class="jp_first_blog_bottom_cont_wrapper">
@@ -77,20 +107,19 @@
 							<div class="jp_blog_bottom_right_cont">
 								<p class="hidden-xs"><a href="#" class="hidden-xs"><i class="fa fa-comments"></i></a></p>
 								<ul>
-									<li>SHARE :</li>
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-									<li class="hidden-xs"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-									<li class="hidden-xs"><a href="#"><i class="fa fa-vimeo"></i></a></li>
+									<li>CHIA SẺ :</li>
+									<li><a href="javascript:void(0);"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="javascript:void(0);"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="javascript:void(0);"><i class="fa fa-pinterest-p"></i></a></li>
+									<li><a href="javascript:void(0);"><i class="fa fa-linkedin"></i></a></li>
+									<li class="hidden-xs"><a href="javascript:void(0);"><i class="fa fa-google-plus"></i></a></li>
+									<li class="hidden-xs"><a href="javascript:void(0);"><i class="fa fa-vimeo"></i></a></li>
 								</ul>
 							</div>
 						</div>
 						<div class="clr"></div>
 					</div>
-				</div>
-				
+				</div>				
 				<?php
 			}
 			?>	

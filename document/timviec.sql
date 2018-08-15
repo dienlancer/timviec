b0d9e795f9dbc62f33cbcdae0823f9b0b39c59de
NguyenKimDien <trietnk01@gmail.com>
2018-08-15 13:58:19 +0700

    cập nhật

--- document/timviec.sql
+++ document/timviec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 15, 2018 at 07:36 AM
+-- Generation Time: Aug 15, 2018 at 08:57 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.1.17
 
